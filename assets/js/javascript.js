d8112ca1a6fed5a2e4a7f50db5c2d9e940cf084b
Marcialas <martynaspalsys@gmail.com>
2022-10-31 12:29:36 +0000

    Add game rules to decide who wins the game

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,3 +4,43 @@ const computerScore = document.getElementById('computer_score');
 const playerScore = document.getElementById('player_score');
 let playerScoreCount = 0;
 let computerScoreCount = 0;
+
+// game rules to decide who wins
+
+let gameRules = {
+    Rock: {
+        Rock: 'draw',
+        Scissors: 'lose',
+        Paper: 'win',
+        Spock: 'win',
+        Lizard: 'lose'
+    },
+    Paper: {
+        Paper: 'draw',
+        Rock: 'lose',
+        Scissors: 'win',
+        Lizard: 'win',
+        Spock: 'lose'
+    },
+    Scissors: {
+        Scissors: 'draw',
+        Paper: 'lose',
+        Rock: 'win',
+        Lizard: 'lose',
+        Spock: 'win'
+    },
+    Lizard: {
+        Scissors: 'win',
+        Paper: 'lose',
+        Rock: 'win',
+        Lizard: 'draw',
+        Spock: 'lose'
+    },
+    Spock: {
+        Scissors: 'lose',
+        Paper: 'win',
+        Rock: 'lose',
+        Lizard: 'win',
+        Spock: 'draw'
+    }
+};
