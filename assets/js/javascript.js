fb1d8a7e6b89cc973761515110f2b767a829e89f
Marcialas <martynaspalsys@gmail.com>
2022-10-31 12:27:53 +0000

    Add global variables to javascript

---
+++ assets/js/javascript.js
@@ -0,0 +1,6 @@
+// global variables 
+const message = document.getElementById("message");
+const computerScore = document.getElementById('computer_score');
+const playerScore = document.getElementById('player_score');
+let playerScoreCount = 0;
+let computerScoreCount = 0;
